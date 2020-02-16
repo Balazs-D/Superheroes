d968104168868bacc168f1aef805dde4ab141d93
Balazs-D <balazs.danyadi@gmail.com>
2020-02-16 22:43:14 +0100

    fetch works / api CORS error

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,25 @@ import Navbar from './layout/Navbar';
 import '../assets/scss/style.scss';
 
 export default function App() {
+
+ 
+
+  const doFetch = async () => {
+    const res = await 
+    axios
+    .get('https://superheroapi.com/api/10218694070596012/1');
+    
+    const database = res.data;
+    console.log(database)
+
+  }
+
+
   return (
     <Router>
       <div>
         <Navbar />
+        <button onClick={doFetch}>FETCH</button>
         <Switch>
           <Route path='/home'></Route>
           <Route path='/library'></Route>
